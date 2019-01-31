f09008ec75bc8dc7e3ab8242530434eef9fb0210
Alex Bugnar <alexbugnar@users.noreply.github.com>
2019-01-31 18:55:17 +0100

    Enemy Script + Folders

---
+++ The Great Rescue/Assets/Scripts/Enemy/RangedEnemy1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemy1 : MonoBehaviour
+{
+
+    private Transform target;
+    private Transform enemyPosition;
+    public float speed;
+    private float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Target").GetComponent<Transform>();
+        enemyPosition = GameObject.FindGameObjectWithTag("RangedEnemy1").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        
+
+      
+    }
+} 
