80ce6bd19879687643c122ab092bbe278fa2982b
HampusPaulsen <47183982+HampusPaulsen@users.noreply.github.com>
2019-02-06 15:33:27 +0100

    Ellie (read below)
    - Made a collision script on Ellie, but had to add a rigidbody to PlayerCharacter for it to work, made it so its not affected by gravity and then froze rotation, Player will not collide with enemies as well but that is probably how we want it anyways

---
+++ The Great Rescue/Assets/Scripts/Dog/GameEndScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndScript : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "PlayerCharacter")
+        {
+            GetComponent<SpriteRenderer>().color = new Color(0.85490f,  0.64706f,  0.12549f);
+        }
+    }
+}
