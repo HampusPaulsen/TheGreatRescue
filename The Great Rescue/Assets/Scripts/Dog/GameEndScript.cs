131205e00fc2198526477fd05e9bc93ff91d5e10
HampusPaulsen <47183982+HampusPaulsen@users.noreply.github.com>
2019-02-06 15:41:55 +0100

    some more comments

--- The Great Rescue/Assets/Scripts/Dog/GameEndScript.cs
+++ The Great Rescue/Assets/Scripts/Dog/GameEndScript.cs
@@ -6,10 +6,11 @@ public class GameEndScript : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D col)
     {
-        //checks if colliding with something named PlayerCharacter, if true then change color of Ellie 2 the RGB value of Gold
+        //checks if colliding with something named PlayerCharacter, if true then change color of Ellie 2 the RGB value of Gold for now, remove line 12 and remove comments on line 13 in final version
         if (col.gameObject.name == "PlayerCharacter")
         {
             GetComponent<SpriteRenderer>().color = new Color(0.85490f,  0.64706f,  0.12549f);
+            // SceneManagement.SceneManager.LoadScene("EndScene);
         }
     }
 }
