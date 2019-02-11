cf944dede06fdd017037611a0def3828e0e8ceea
HampusPaulsen <47183982+HampusPaulsen@users.noreply.github.com>
2019-02-11 13:28:04 +0100

    Made Measuring Cube and made a boundary for the background part with a collider on it

---
+++ The Great Rescue/Assets/Scripts/Misc/MakeTransparent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeTransparent : MonoBehaviour {
+
+    // Use this for initialization
+    
+
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+
+    }
+}
