2d75bf2e006fa2fe3a41ed057727ea4ecead480f
HampusPaulsen <47183982+HampusPaulsen@users.noreply.github.com>
2019-02-18 00:47:05 +0100

    plan B game

---
+++ The Great Rescue/Assets/PlaceholderPlayerBulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderPlayerBulletScript : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //checks if colliding with something named PlayerCharacter
+        if (col.gameObject.tag == "RangedEnemy1")
+        {
+            {
+
+                //prints in console what it collides with 4 testing
+                print("Player arrow collided with: " + col.gameObject.name);
+                Destroy(col.gameObject);
+
+
+
+            }
+        }
+    }
+}
