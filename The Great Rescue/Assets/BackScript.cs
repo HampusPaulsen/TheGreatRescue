196a1621769cff216741dca4d73656869e0fd838
HampusPaulsen <47183982+HampusPaulsen@users.noreply.github.com>
2019-03-13 13:50:59 +0100

    Scene Changes

---
+++ The Great Rescue/Assets/BackScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadStartScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("0. StartScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
