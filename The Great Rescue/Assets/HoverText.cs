f3a444330762332f454216e72fe4d30c1b786385
HampusPaulsen <47183982+HampusPaulsen@users.noreply.github.com>
2019-03-22 13:09:52 +0100

    instructions

---
+++ The Great Rescue/Assets/HoverText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverText : MonoBehaviour
+{
+
+    public Renderer menuObject;
+
+    void OnMouseEnter()
+    {
+        menuObject.enabled = true;
+    }
+    void OnMouseExit()
+    {
+
+        menuObject.enabled = false;
+    }
+}
